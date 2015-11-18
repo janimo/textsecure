ba80434c875248cfee677a075cd08dbecd10d11f
Jani Monoses <jani@ubuntu.com>
2015-11-18 11:03:44 +0200

    Add timestamp to log messages.

--- textsecure.go
+++ textsecure.go
@@ -222,6 +222,11 @@ func setupLogging() {
 	default:
 		log.SetLevel(log.ErrorLevel)
 	}
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: "2006/01/02 15:04:05",
+	})
 }
 
 // Setup initializes the package.
