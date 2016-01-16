852f51b5e4cbeccfb455652dd276aa0d213e317f
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-01-16 12:07:42 -0800

    include ServerName in TLS configuration
    This is required for Go to establish a TLS connection unless
    InsecureSkipVerify is set.

--- transport.go
+++ transport.go
@@ -8,6 +8,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
 
@@ -58,10 +59,19 @@ func getProxy(req *http.Request) (*url.URL, error) {
 }
 
 func newHTTPTransporter(baseURL, user, pass string) *httpTransporter {
+	u, _ := url.Parse(baseURL)
+	host, _, err := net.SplitHostPort(u.Host)
+	if err != nil {
+		host = u.Host
+	}
+
 	client := &http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{RootCAs: rootCA},
-			Proxy:           getProxy,
+			TLSClientConfig: &tls.Config{
+				RootCAs:    rootCA,
+				ServerName: host,
+			},
+			Proxy: getProxy,
 		},
 	}
 
